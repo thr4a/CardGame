4643dcde58480807e73dee91ed77d11b63ae38b1
thr4a <aklitahar2@gmail.com>
2022-11-12 20:54:13 +0100

    ajout de la classe Player et finalisation du modèle

---
+++ src/com/openclassrooms/cardgame/model/Player.java
@@ -0,0 +1,24 @@
+package com.openclassrooms.cardgame.model;
+
+public class Player {
+    private String name;
+    private Hand hand;
+
+    public Player(String name) {
+        super();
+        this.name = name;
+        this.hand = new Hand();
+    }
+
+    public void addCardToHand(PlayingCard pc) {
+        this.hand.addCard(pc);
+    }
+
+    public PlayingCard getcard(int index) {
+        return this.hand.getCard(index);
+    }
+
+    public PlayingCard removeCard() {
+        return this.hand.removeCard();
+    }
+}
